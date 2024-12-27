aa1eb0e837f27c55f2cf04ed667d0791828aecf3
B RAM SAI TEJA <saitejain8055@gmail.com>
2024-12-27 18:21:24 +0530

    onprogress2

--- coffe.py
+++ coffe.py
@@ -1,7 +1,56 @@
 #we are going to build a coffe shop
-def order():
-    pass
+def orderlist():
+    print("\n                ORDER LIST                   ")
+    print("\n1. Idly      - 5$         2. Idlysambar    - 6$")
+    print("\n3. Gare      - 7$         4. Dosa          - 6$")
+    print("\n5. Bajji     - 8$         6. Puri          - 10$")
+    print("\n7. Onion Dosa- 10$        8. Masala Dosa   - 12$")
+    print("\n***            Choose your Order             ***")
+    total = 0
+    additem = input('Add items (y/n) : ')
+    while additem.upper() == 'Y':
+        a = int(input("Enter your choice : "))
+        if a == 1:
+            acost = 5
+            print(f'Idly  - {acost}$')
+            total += acost
+            print(f'Total - {total}$')
+            additem = input('add another (y/n) : ')
+        elif a == 2:
+            bcost = 6
+            print(f'Idlysambar - {bcost}$')
+            total += bcost
+            print(f'Total      - {total}$')
+            additem = input('add another (y/n) : ')
+        elif additem.upper == 'N':
+            break
+        elif additem.upper() != 'y':
+            print("Choose correctly")
+            additem = 'y'
+    print(f"Yout total is {total}$")
 
+
+
+    
+def orderdenaid():
+    print('\n     Thank You...     ')
+    exit()
+def order():
+    decision = input('Do you wanna order?(y/n) : ')
+    if decision.upper() == 'Y':
+        orderlist()
+    elif decision.upper() == 'N':
+        orderdenaid()
+
+def welcome():
+    print(f"Hello SaiTeja🙋🏻... ")
+    print("\n***   WELCOME TO SRI BALAJI FAST FOODS   *** ")
+    print("\n                ORDER LIST                   ")
+    print("\n1. Idly      - 5$         2. Idlysambar    - 6$")
+    print("\n3. Gare      - 7$         4. Dosa          - 6$")
+    print("\n5. Bajji     - 8$         6. Puri          - 10$")
+    print("\n7. Onion Dosa- 10$        8. Masala Dosa   - 12$")
+    order()
 def login():
     global username 
     global password
@@ -14,26 +63,29 @@ def checklogin():
     global username
     global password
     if username.upper() == 'RAMSAITEJA' and password == '123456789':
-        print('logged in')
+        print(f'logged in...')
+        welcome()
+
     else:
         print('Enter the correct credentials')
-    pass
-    
+    login()
 
 def signup():
-    pass
+    print('Server under working process....')
+    login()
 
 print("*** WELCOME TO BALAJI FAST FOODS ***")
 
-print("        1.LOGIN or 2.SIGNUP       ")
+
 while True:
+    print("        1.LOGIN or 2.SIGNUP       ")
     l = input('Enter Your Option : ')
     if l == '1' or l.upper() == 'LOGIN':
         login()
         break
     elif l == '2' or l.upper() == 'SIGNUP':
         signup()
-        break
+        continue
     else:
         print('Choose option correctly!!!')
         continue
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-name = '123456789'
-print(name.upper())
+print(f"Hello SaiTeja🙋🏻... ")
+print("\n***   WELCOME TO SRI BALAJI FAST FOODS   *** ")
+print("\n                ORDER LIST                   ")
+print("\n1. Idly      - 5$         2. Idlysambar    - 6$")
+print("\n3. Gare      - 7$         4. Dosa          - 6$")
+print("\n5. Bajji     - 8$         6. Puri          - 10$")
+print("\n7. Onion Dosa- 10$        8. Masala Dosa   - 12$")
+
+
